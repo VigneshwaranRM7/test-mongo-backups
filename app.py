89fbab0534eaee084b124303e56a421c0233395e
Vicky <85546041+VigneshwaranRM7@users.noreply.github.com>
2025-09-19 13:47:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -175,3 +175,8 @@ def backup_all_databases():
         results.append(result)
     
     return jsonify(results)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
